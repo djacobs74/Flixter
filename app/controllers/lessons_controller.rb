f8b07cb8a573a4c83b8f4d5427d6693e1e392676
vagrant <vagrant@precise32.(none)>
2016-07-08 22:04:46 +0000

    Lesson 27: [Challenge] - Require users to be enrolled in a course to view the lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,27 @@
 class LessonsController < ApplicationController
+    
+    before_action :authenticate_user!
+    # enrolled in course?
+    before_action :require_enrolled_for_current_lesson, only: [:show]
+    #
+
   def show
   end
 
   private
+
+  # enrolled method
+  def require_enrolled_for_current_lesson
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: "You are not enrolled in this course, please enroll", status: :unauthorized
+    end
+  end
+
+ #
+
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
 end
+ 
